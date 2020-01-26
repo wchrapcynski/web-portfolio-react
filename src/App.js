956803e912b70154f7e3046e1e3f8a6b23035829
William Chrapcynski <wchrapcynski@gmail.com>
2020-01-25 21:10:05 -0500

    Projects component is mostly done. Some links need to finished

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import React from 'react';
-import './App.css';
-import './bootstrap.min.css';
-import Projects from './components/Projects'
+import React from "react";
+import "./App.css";
+import "./bootstrap.min.css";
+import Projects from "./components/Projects";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Projects></Projects>
-      </header>
-    </div>
-  );
+  const softwareProjects = require("./data/projects.json");
+
+  const displayProjects = softwareProjects.map((data, key) => {
+    return (
+      <Projects
+        key={key}
+        name={data.name}
+        description={data.description}
+        appLink={data.appLink}
+        image={data.image}
+      ></Projects>
+    );
+  });
+
+  return <div className="App">{displayProjects}</div>;
 }
 
 export default App;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,34 +1,38 @@
-import React from 'react';
-import './Projects.css'
+import React from "react";
+import "./Projects.css";
 
-function Projects() {
-
-	return (
+function Projects(props) {
+  return (
     <div className="flip-card card">
       <div className="flip-card-inner">
         <div className="flip-card-front card-body">
-          <a href="http://mousetrivia.magicaltech.xyz/" target="_blank">
+          <a
+            href="http://mousetrivia.magicaltech.xyz/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <img
-              src={require("../images/mousetrivia-flip.png")}
+              src={require(`../images/${props.image}`)}
               className="card-img-top flip-image img-fluid"
               alt="Avatar"
             />
           </a>
         </div>
         <div className="flip-card-back">
-          <h2>Mouse Fan Trivia</h2>
+          <h2>{props.name}</h2>
           <p>
-            Trivia game created with JS, CSS, and HTML.
+            {props.description}
             <br />
             <a
               href="https://github.com/wchrapcynski/trivia_game"
               target="_blank"
+              rel="noopener noreferrer"
             >
-            Repo
+              Repo
             </a>
             {" - "}
-            <a href="http://mousetrivia.magicaltech.xyz/" target="_blank">
-            Web App
+            <a href={props.appLink} target="_blank" rel="noopener noreferrer">
+              Web App
             </a>
           </p>
         </div>
