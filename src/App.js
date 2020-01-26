b7ada01f95a1eec5651bad281fa73855611f19d8
William Chrapcynski <wchrapcynski@gmail.com>
2020-01-26 18:59:43 -0500

    Cleaned up and added to Projects component

--- src/App.js
+++ src/App.js
@@ -4,28 +4,21 @@ import "./bootstrap.min.css";
 import Projects from "./components/Projects";
 
 function App() {
-  const softwareProjects = require("./data/projects.json");
-  const displayProjects = softwareProjects.map((data, key) => {
-    return (
-      <Projects
-        key={key}
-        name={data.name}
-        description={data.description}
-        appLink={data.appLink}
-        image={data.image}
-        repoFrontLink={data.repoFrontLink}
-        repoBackLink={data.repoBackLink}
-      ></Projects>
-    );
-  });
+	const softwareProjects = require("./data/projects.json");
+	const displayProjects = softwareProjects.map((data, key) => {
+		return (
+			<Projects key={key} name={data.name} description={data.description} appLink={data.appLink}
+			image={data.image} repoFrontLink={data.repoFrontLink} repoBackLink={data.repoBackLink} />
+			);
+		});
 
-  return (
+return (
     <div className="App">
-      <div className="portfolio-projects card-group">
-        <div className="headline-text">Software Engineering Projects</div>
-        <hr className="rule" />
-        <div className="card-row">{displayProjects}</div>
-      </div>
+    	<div className="portfolio-projects card-group">
+			<div className="headline-text">Software Engineering Projects</div>
+			<hr className="rule" />
+			<div className="card-row">{displayProjects}</div>
+    	</div>
     </div>
   );
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,63 +4,52 @@ import "./Projects.css";
 function Projects(props) {
   function displayRepos() {
     if (props.repoBackLink) {
-      return (
-        <React.Fragment>
-          <a
-            href={props.repoFrontLink}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Front End
-          </a>{" - "}
-          <a
-            href={props.repoBackLink}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Back End
-          </a>
-        </React.Fragment>
-      );
+      	return (
+		<React.Fragment>
+			<a href={props.repoFrontLink} target="_blank" rel="noopener noreferrer">Front End</a>{" - "}
+			<a href={props.repoBackLink} target="_blank" rel="noopener noreferrer">Back End</a>
+		</React.Fragment>
+		);
     } else {
-      return (
-        <a href={props.repoFrontLink} target="_blank" rel="noopener noreferrer">
-          Repo
-        </a>
-      );
-    }
-  }
+    	return (
+		<a href={props.repoFrontLink} target="_blank" rel="noopener noreferrer">Repo</a>
+		);}
+	}
 
   return (
-    <div className="flip-card card">
-      <div className="flip-card-inner">
-        <div className="flip-card-front card-body">
-          <a
-            href="http://mousetrivia.magicaltech.xyz/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src={require(`../images/${props.image}`)}
-              className="card-img-top flip-image img-fluid"
-              alt="Avatar"
-            />
-          </a>
-        </div>
-        <div className="flip-card-back">
-          <h2>{props.name}</h2>
-          <p>
-            {props.description}
-            <br />
-            {displayRepos()}
-            <br />
-            <a href={props.appLink} target="_blank" rel="noopener noreferrer">
-              Web App
+    <React.Fragment>
+      <div className="flip-card card">
+        <div className="flip-card-inner">
+          <div className="flip-card-front card-body">
+            <a href="http://mousetrivia.magicaltech.xyz/" target="_blank" rel="noopener noreferrer">
+			  	<img src={require(`../images/${props.image}`)} className="card-img-top flip-image img-fluid" 
+			  	alt="Avatar" />
             </a>
-          </p>
+          </div>
+          <div className="flip-card-back">
+            <h2>{props.name}</h2>
+            <p>
+              	{props.description}
+              	<br />
+              	{displayRepos()}
+             	<br />
+              	{props.appLink ? <a href={props.appLink} target="_blank" rel="noopener noreferrer">Web App</a>: ""}
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+      <div className="flip-card-desc">
+        <h2>{props.name}</h2>
+        <p>
+        	{props.description}
+        	<br />
+        	{displayRepos()}
+        	<br />
+			{props.appLink ? <a href={props.appLink} target="_blank" rel="noopener noreferrer">
+            Web App</a>: ""}
+        </p>
+      </div>
+    </React.Fragment>
   );
 }
 
