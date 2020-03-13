2117c060743c3da144462dc4d809d5642e8bac3e
William Chrapcynski <wchrapcynski@gmail.com>
2020-03-13 16:29:07 -0400

    Added text description for icon links

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -6,23 +6,29 @@ function SocialMedia(props) {
   if (props.external) {
     return (
       <>
-        <div className='social-media-img'>
-          <a href={props.link} target='_blank' rel='noopener noreferrer'>
+        <div className="social-media-img">
+          <a href={props.link} target="_blank" rel="noopener noreferrer">
             <img src={require(`../images/${props.image}`)} alt={props.name} />
           </a>
+          <div className="social-media-text">
+            {props.name.charAt(0).toUpperCase() + props.name.slice(1)}
+          </div>
         </div>
       </>
     );
   } else {
     return (
       <>
-        <div className='social-media-img'>
+        <div className="social-media-img">
           <Link
             to={props.link}
-            target='_blank'
-            rel='noopener noreferrer'
+            target="_blank"
+            rel="noopener noreferrer"
             download>
             <img src={require(`../images/${props.image}`)} alt={props.name} />
+            <div className="social-media-text">
+              {props.name.charAt(0).toUpperCase() + props.name.slice(1)}
+            </div>
           </Link>
         </div>
       </>
